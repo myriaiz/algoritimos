884d8dde16b9fcf7bbf04d83d4b1ddb31c077c7f
myriaiz <138396898+myriaiz@users.noreply.github.com>
2023-07-04 20:39:04 -0300

    Create lista4 Q1 arquivo.cpp

---
+++ lista4 Q1 arquivo.cpp
@@ -0,0 +1,11 @@
+/*NOME:MYRIA IZABELE ALVES DE CARVALHO. TURMA:IF23.
+1. Faça um programa que leia uma string e a imprima na tela*/
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main() {
+  char mu[33];
+  cout << "mu:" << endl;
+  fgets(mu,33,stdin);
+  cout << mu << endl;
+}
